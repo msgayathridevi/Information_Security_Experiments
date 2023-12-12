958d1ff35b231f5b772acf3873c4ce6b0fc55460
msgayathridevi <gayudevi.2002@gmail.com>
2023-12-12 16:52:38 +0530

    alter file names

---
+++ SQL_Injection.sql
@@ -0,0 +1,10 @@
+SQL CODE:
+-- CREATE TABLE sqli(username TEXT, pass NOT NULL, hack);
+
+-- INSERT INTO sqli(username, pass, hack)
+-- VALUES("bags", "bags1212", "fbf880974e83a1a20ac89922500d32fc");
+
+-- SELECT * FROM sqli;
+
+
+' OR 1=1;--
